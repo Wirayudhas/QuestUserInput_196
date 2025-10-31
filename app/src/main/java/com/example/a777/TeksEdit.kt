2c1d96e625c27451701ac948aff9edb15f061cb2
arthur <arturwirayuda@gmail.com>
2025-10-31 10:33:16 +0700

    membuat teksfield

--- app/src/main/java/com/example/a777/TeksEdit.kt
+++ app/src/main/java/com/example/a777/TeksEdit.kt
@@ -30,3 +30,32 @@ import javax.xml.namespace.QName
 
 @Composable
 fun FormDataDiri(modifier: Modifier
+) {
+    //variabel-variabel untuk mengingat nilai nasukan dari keyboard
+    var textNama by remember { mutableStateOf("") }
+    var textAlamat by remember { mutableStateOf("") }
+    var textJK by remember { mutableStateOf("") }
+
+    //variabel-variabel untuk menyimpan data yang diperoleh dari komponen UI
+    var nama by remember { mutableStateOf("") }
+    var alamat by remember { mutableStateOf("") }
+    var jenis by remember { mutableStateOf("") }
+
+    val gender:List<String> = listOf("Laki-Laki", "Perempuan")
+
+    Column(modifier = Modifier.padding(top = 50.dp),
+        verticalArrangement = Arrangement.Top,
+
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        OutlinedTextField(
+            value = textNama,
+            singleLine = true,
+            shape = MaterialTheme.shapes.large,
+            modifier = Modifier.width(250.dp),
+            label = { Text(text = "Nama Lengkap") },
+            onValueChange = {
+                textNama = it
+            }
+        )
+
